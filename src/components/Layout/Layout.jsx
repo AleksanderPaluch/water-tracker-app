123ea66ca0da0855dd5f269e28c0f270f8f873ab
Sashiy1 <aleksander.paluch@wp.pl>
2024-07-08 14:34:35 +0200

    prop-types added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import {  Suspense } from 'react'
+import PropTypes from 'prop-types';
 
 const Layout = ({children}) => {
   return (
@@ -7,6 +8,10 @@ const Layout = ({children}) => {
     {children}
     </Suspense>
   )
+}
+
+Layout.propTypes = {
+    children: PropTypes.element
 }
 
 export default Layout
