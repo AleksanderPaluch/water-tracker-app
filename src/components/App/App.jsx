0c4d6e8102f2091322f7dda675b36d526cb099f3
Sashiy1 <aleksander.paluch@wp.pl>
2024-07-08 13:43:37 +0200

    router added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../pages/HomePage/HomePage";
+import SignInPage from "../../pages/SignInPage/SignInPage";
+import SignUpPage from "../../pages/SignUpPage/SignUpPage";
+import TrackerPage from "../../pages/TrackerPage/TrackerPage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
-import './App.css'
+
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-  <h1>aquaTrack</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/tracker" element={<TrackerPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,11 @@
+
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <h1>SignUpPage</h1>
+    </div>
+  )
+}
+
+export default SignUpPage
