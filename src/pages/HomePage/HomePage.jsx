0c16e493f1d19f79e3ebd834f0f2794a23a9bc16
Sashiy1 <aleksander.paluch@wp.pl>
2024-07-16 01:29:43 +0200

    welcome section added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
 import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
 import Page from "../../components/Page/Page";
+import { WelcomeSection } from "../../components/WelcomeSection/WelcomeSection";
 
 const HomePage = () => {
   return (
     <Page>
+      <WelcomeSection />
       <AdvantagesSection />
-    
-   
     </Page>
   );
 };
