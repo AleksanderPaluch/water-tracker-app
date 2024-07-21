0569ec9cf38b70256b40df6997bd076eb1113e7d
Sashiy1 <aleksander.paluch@wp.pl>
2024-07-21 15:08:48 +0200

    daily box added

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,4 +1,5 @@
 import Icon from "../Icon/Icon"
+import WaterDailyNorma from "../WaterDailyNorma/WaterDailyNorma"
 import css from "./WaterMainInfo.module.css"
 
 
@@ -8,6 +9,7 @@ const WaterMainInfo = () => {
   return (
     
     <div className={css.waterInfoBox}>
+    <WaterDailyNorma />
        
     <button className={css.addWaterBtn}>  <Icon width="16" height="16" iconName="plus" styles={css.plusIcon} />
       Add Water</button>
