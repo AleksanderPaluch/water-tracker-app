b821e48f3745ed9c8075f30f58c36076073f0d22
Sashiy1 <aleksander.paluch@wp.pl>
2024-07-23 14:27:14 +0200

    addWater component added

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,10 +1,15 @@
+import AddWaterBtn from "../AddWaterBtn/AddWaterBtn"
 import css from "./WaterDetailedInfo.module.css"
 
 const WaterDetailedInfo = () => {
+
+  const user = "Alexander"
+
   return (
       
     <div className={css.waterInfoBox}>
-      
+      <p className={css.welcomeText}>Hello <span className={css.welcomeName}>, {user}!</span></p>
+      <AddWaterBtn isBig={false}/>
     </div>
   )
 }
--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,32 +1,14 @@
-import { useState } from "react";
-import BaseModal from "../BaseModal/BaseModal";
-import Icon from "../Icon/Icon";
+import AddWaterBtn from "../AddWaterBtn/AddWaterBtn";
 import WaterDailyNorma from "../WaterDailyNorma/WaterDailyNorma";
 import WaterProgressBar from "../WaterProgressBar/WaterProgressBar";
 import css from "./WaterMainInfo.module.css";
 
 const WaterMainInfo = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
   return (
     <div className={css.waterInfoBox}>
       <WaterDailyNorma />
       <WaterProgressBar />
-
-      <button onClick={openModal} className={css.addWaterBtn}>
-       
-        <Icon width="16" height="16" iconName="plus" styles={css.plusIcon} />
-        Add Water
-      </button>
-      <BaseModal isOpen={modalIsOpen} onClose={closeModal}></BaseModal>
+      <AddWaterBtn  />
     </div>
   );
 };
