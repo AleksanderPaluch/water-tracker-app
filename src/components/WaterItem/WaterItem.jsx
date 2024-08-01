3822cb3d26ed64b075ab2077260c2ec618a4784f
Sashiy1 <aleksander.paluch@wp.pl>
2024-08-01 12:06:49 +0200

    modals logics added

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import Icon from "../Icon/Icon";
 import css from "./WaterItem.module.css";
+import BaseModal from "../BaseModal/BaseModal";
 
 const WaterItem = () => {
+  const [activeModal, setActiveModal] = useState(null);
+
+  const handleButtonClick = (modalName) => {
+    setActiveModal(modalName);
+  };
+
+  function closeModal() {
+    setActiveModal(null);
+  }
+
   return (
     <div className={css.waterItemBox}>
       <Icon width="38   " height="38" iconName="glass" styles={css.glass} />
@@ -10,9 +22,35 @@ const WaterItem = () => {
         <p className={css.text}>7:00 AM</p>
       </div>
       <div className={css.iconsBox}>
-        <Icon width="14" height="14" iconName="edit" styles={css.icons} />
-        <Icon width="14" height="14" iconName="trash" styles={css.icons} />
+        <button
+          className={css.iconBtn}
+          type="button"
+          onClick={() => handleButtonClick("edit")}
+        >
+          {" "}
+          <Icon width="14" height="14" iconName="edit" styles={css.icons} />
+        </button>
+        <button
+          className={css.iconBtn}
+          type="button"
+          onClick={() => handleButtonClick("delete")}
+        >
+          {" "}
+          <Icon width="14" height="14" iconName="trash" styles={css.icons} />
+        </button>
       </div>
+      {activeModal === "edit" && (
+        <BaseModal isOpen={true} onClose={closeModal}>
+          {" "}
+          edit modal
+        </BaseModal>
+      )}
+      {activeModal === "delete" && (
+        <BaseModal isOpen={true} onClose={closeModal}>
+          {" "}
+          delete modal
+        </BaseModal>
+      )}
       
     </div>
   );
