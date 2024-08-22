4eb1f24b5c17553b7509a49e385dd31c15f89547
Sashiy1 <aleksander.paluch@wp.pl>
2024-08-22 16:53:32 +0200

    water modal/form split

---
+++ src/components/WaterForm/WaterForm.jsx
@@ -0,0 +1,111 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+import * as Yup from "yup";
+import css from "./WaterForm.module.css";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const validationSchema = Yup.object().shape({
+  water: Yup.number()
+    .required("Please enter a valid number")
+    .typeError("Please enter a valid number")
+    .min(50, "The minimum amount is 50 ml")
+    .max(1000, "The maximum amount is 1000 ml"),
+});
+
+const WaterForm = ({
+  isEdit = false,
+  initialValues,
+  onSubmit,
+  editAmount,
+}) => {
+  const [amount, setAmount] = useState(editAmount || 150);
+
+  return (
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+    >
+      {({ setFieldValue, values }) => (
+        <Form className={css.form}>
+          <p className={css.labelText}>Amount of water:</p>
+          <div className={css.calcBox}>
+            <button
+              type="button"
+              onClick={() => {
+                const newAmount = Math.max(amount - 50, 50);
+                setAmount(newAmount);
+                setFieldValue("water", newAmount);
+              }}
+              className={css.calcBtn}
+            >
+              <CiCircleMinus className={css.icon} />
+            </button>
+            <div className={css.waterAmountBox}>
+              {isEdit ? editAmount : amount}
+            </div>
+            <button
+              type="button"
+              onClick={() => {
+                const newAmount = Math.min(amount + 50, 1000);
+                setAmount(newAmount);
+                setFieldValue("water", newAmount);
+              }}
+              className={css.calcBtn}
+            >
+              <CiCirclePlus className={css.icon} />
+            </button>
+          </div>
+          <label className={css.label}>
+            <span className={css.labelText}>Recording time:</span>
+            <Field className={css.formInput} type="time" name="time" />
+          </label>
+          <label className={css.label}>
+            <span className={css.labelBoldText}>
+              Enter the value of the water used:
+            </span>
+            <Field
+              className={css.formInput}
+              type="number"
+              name="water"
+              autoComplete="off"
+              step="50"
+              value={amount}
+              onChange={(e) => {
+                const newAmount = Math.min(
+                  Math.max(Number(e.target.value), 50),
+                  1000
+                );
+                setAmount(newAmount);
+                setFieldValue("water", newAmount);
+              }}
+            />
+          </label>
+          <ErrorMessage
+            name="water"
+            component="span"
+            className={css.errorMessage}
+          />
+          <button
+            className={css.formBtn}
+            type="submit"
+            title="click to add amount of water"
+            aria-label="add amount of water"
+          >
+            Save
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+WaterForm.propTypes = {
+  isEdit: PropTypes.bool,
+  initialValues: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  editAmount: PropTypes.number,
+};
+
+export default WaterForm;
--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,22 +1,11 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import * as Yup from "yup";
-import css from "./WaterModal.module.css";
+
 import PropTypes from "prop-types";
-import { CiCirclePlus } from "react-icons/ci";
-import { CiCircleMinus } from "react-icons/ci";
-import { useState } from "react";
+import css from "./WaterModal.module.css";
 import Icon from "../Icon/Icon";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { apiAddWater, apiEditWater } from "../../redux/water/operations";
-
-const validationSchema = Yup.object().shape({
-  water: Yup.number()
-    .required("Please enter a valid number")
-    .typeError("Please enter a valid number")
-    .min(50, "The minimum amount is 50 ml")
-    .max(1000, "The maximum amount is 1000 ml"),
-});
+import WaterForm from "../WaterForm/WaterForm"; // Імпорт нового компоненту форми
 
 const INITIAL_FORM_DATA = {
   time: "",
@@ -30,8 +19,8 @@ const WaterModal = ({
   editAmount,
   id,
 }) => {
-  const [amount, setAmount] = useState(150);
   const dispatch = useDispatch();
+
   const getCurrentTime = () => {
     const now = new Date();
     return now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
@@ -69,7 +58,6 @@ const WaterModal = ({
   return (
     <div className={css.waterModalBox}>
       <button type="button" onClick={closeModal} className={css.iconExitBtn}>
-        {" "}
         <Icon width="24" height="24" iconName="x" className={css.iconExit} />
       </button>
 
@@ -79,82 +67,17 @@ const WaterModal = ({
       <p className={css.text}>
         {!isEdit ? "Choose a value" : "Correct entered data:"}
       </p>
-      <Formik
-        validationSchema={validationSchema}
+
+      <WaterForm
+        isEdit={isEdit}
         initialValues={{
           ...INITIAL_FORM_DATA,
-          time: isEdit ? editTime : new Date(),
-          water: isEdit ? editAmount : amount,
+          time: isEdit ? editTime : getCurrentTime(),
+          water: isEdit ? editAmount : 150,
         }}
         onSubmit={handleAddingWater}
-      >
-        {({ setFieldValue }) => (
-          <Form className={css.form}>
-            <p className={css.labelText}>Amount of water:</p>
-            <div className={css.calcBox}>
-              <button
-                type="button"
-                onClick={() => {
-                  const newAmount = Math.max(amount - 50, 50);
-                  setAmount(newAmount);
-                  setFieldValue("water", newAmount);
-                }}
-                className={css.calcBtn}
-              >
-                <CiCircleMinus className={css.icon} />
-              </button>
-              <div className={css.waterAmountBox}>
-                {isEdit ? editAmount : amount}
-              </div>
-              <button
-                type="button"
-                onClick={() => {
-                  const newAmount = Math.min(amount + 50, 1000);
-                  setAmount(newAmount);
-                  setFieldValue("water", newAmount);
-                }}
-                className={css.calcBtn}
-              >
-                <CiCirclePlus className={css.icon} />
-              </button>
-            </div>
-            <label className={css.label}>
-              <span className={css.labelText}>Recording time:</span>
-              <Field
-                className={css.formInput}
-                type="string"
-                name="time"
-                readOnly
-                value={getCurrentTime()}
-              />
-            </label>
-            <label className={css.label}>
-              <span className={css.labelBoldText}>
-                Enter the value of the water used:
-              </span>
-              <Field
-                className={css.formInput}
-                type="string"
-                name="water"
-                autoComplete="off"
-              />
-            </label>
-            <ErrorMessage
-              name="water"
-              component="span"
-              className={css.errorMessage}
-            />
-            <button
-              className={css.formBtn}
-              type="submit"
-              title="click to add amount of water"
-              aria-label="add amount of water"
-            >
-              Save
-            </button>
-          </Form>
-        )}
-      </Formik>
+        editAmount={editAmount}
+      />
     </div>
   );
 };
@@ -164,6 +87,7 @@ WaterModal.propTypes = {
   closeModal: PropTypes.func,
   editTime: PropTypes.string,
   editAmount: PropTypes.number,
+  id: PropTypes.any, 
 };
 
 export default WaterModal;
