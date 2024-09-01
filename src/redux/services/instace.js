1f81693d9cc224e4def24aba4a5036c07f962f2d
Sashiy1 <aleksander.paluch@wp.pl>
2024-09-01 10:27:41 +0200

    water remove after logout

--- src/redux/services/instace.js
+++ src/redux/services/instace.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://water-tracker-app-3d8d0b109609.herokuapp.com/",
+  // baseURL: "https://water-tracker-app-3d8d0b109609.herokuapp.com/",
+  baseURL: "http://localhost:3000/",
   withCredentials: true,
 });
 
